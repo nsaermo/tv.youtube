8200f8ed4fd4dc228d8640068e6a28b4ee0d3826
nsaermo <79844066+nsaermo@users.noreply.github.com>
2024-05-05 16:27:23 +0300

    Update AL-JAZEERA-MUBASHER.sh

--- sh/AL-JAZEERA-MUBASHER.sh
+++ sh/AL-JAZEERA-MUBASHER.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 MBC-Action.py > ../../live/AL-JAZEERA-MUBASHER.m3u8
+python3 AL-JAZEERA-MUBASHER.py > ../../live/AL-JAZEERA-MUBASHER.m3u8
 
 echo MBC-Action grabbed
