8ec97a8c3fa26d88000c78f45c7f09e950eed740
nsaermo <79844066+nsaermo@users.noreply.github.com>
2024-05-06 18:05:59 +0300

    Update and rename Business.sh to Busines.sh

--- sh/Busines.sh
+++ sh/Busines.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 Al Arabiya Business.py > ../../live/Business.m3u8
+python3 Busines.py > ../../live/Business.m3u8
 
 echo AL-JAZEERA-MUBASHER grabbed
