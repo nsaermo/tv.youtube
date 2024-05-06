114baf55cffebac01603804295d0bc34429e82d6
nsaermo <79844066+nsaermo@users.noreply.github.com>
2024-05-06 18:59:41 +0300

    Update and rename ch10.sh to ch10-franc24-ar.sh

--- sh/ch10-franc24-ar.sh
+++ sh/ch10-franc24-ar.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 ch10.py > ../../live/ch10.m3u8
+python3 ch10-franc24-ar.py > ../../live/ch10-franc24-ar.m3u8
 
 echo ch10 grabbed
