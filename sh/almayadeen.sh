0cc98479af3da6956ef425c1d2025e5a17f950bc
nsaermo <79844066+nsaermo@users.noreply.github.com>
2024-05-07 18:43:45 +0300

    Update and rename iraq-news.sh to almayadeen.sh

--- sh/almayadeen.sh
+++ sh/almayadeen.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 iraq-news.py > ../../live/iraq-news.m3u8
+python3 almayadeen.py > ../../live/almayadeen.m3u8
 
-echo iraq-news grabbed
+echo almayadeen grabbed
