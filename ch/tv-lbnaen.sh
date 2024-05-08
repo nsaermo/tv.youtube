ea83633dbf57cec972d4a3f0cd2a808afb047879
nsaermo <79844066+nsaermo@users.noreply.github.com>
2024-05-09 02:03:56 +0300

    Update tv-lbnaen.sh

--- ch/tv-lbnaen.sh
+++ ch/tv-lbnaen.sh
@@ -2,5 +2,4 @@
 ~~ FORMAT: <channel name> | <group name> | <logo> | <tvg-id>
 
 #EXTINF:-1 tvg-id="" tvg-logo="https://www.teleliban.com.lb/images/telelogo.png" ,تلفزيون لبنان
-#EXTVLCOPT:http-referrer=https://www.teleliban.com.lb/live
-https://cdn.catiacast.video/abr/ed8f807e2548db4507d2a6f4ba0c4a06/playlist.m3u8
+https://raw.githubusercontent.com/nsaermo/NSAER.TV-channels/main/assets/offlintv.m3u8
