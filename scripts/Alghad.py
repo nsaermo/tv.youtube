a25dd56f5c965557663032f7ee1164e6774fafde
nsaermo <79844066+nsaermo@users.noreply.github.com>
2024-05-09 01:37:33 +0300

    Update Alghad.py

--- scripts/Alghad.py
+++ scripts/Alghad.py
@@ -14,12 +14,12 @@ def grab(url):
     response = s.get(url, timeout=15).text
     if '.m3u8' not in response:
         if windows:
-            print('https://www.youtube.com/channel/UCH4UNKBHSNwstan99YDoCVg/live')
+            print('https://raw.githubusercontent.com/nsaermo/NSAER.TV-channels/main/assets/offlintv.m3u8')
             return
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('https://www.youtube.com/channel/UCH4UNKBHSNwstan99YDoCVg/live')
+            print('https://raw.githubusercontent.com/nsaermo/NSAER.TV-channels/main/assets/offlintv.m3u8')
             return
     end = response.find('.m3u8') + 5
     tuner = 100
